564374697d4f24c57fbf449221f39777290228ae
projeksainsdata <projeksainsdata@gmail.com>
2024-02-07 09:43:10 +0700

    Third

--- server.js
+++ server.js
@@ -190,54 +190,43 @@ server.post("/signin", (req, res) => {
 })
 
 server.post("/google-auth", async (req, res) => {
-    let { access_token } = req.body;
-  
-    getAuth()
-    .verifyIdToken(access_token)
-    .then(async (decodedUser) => {
-      let { email, name, picture } = decodedUser;
-      console.log(decodedUser)
-      picture = picture.replace("s96-c", "s384-c");
-  
-      let user = await User.findOne({ "personal_info.email": email }).select("personal_info.fullname personal_info.username personal_info.profile_img google_auth")
-      .then((u) => {
-        return u || null
-      })
-      .catch(err => {
-        console.log('error server',err)
-        return res.status(500).json({"error": err.message})
-      })
-  
-      if(user){//login
-        if(!user.google_auth){
-          return res.status(403).json({"error" : "This email was signed up without google. Please log in with password to access the account"})
+    try {
+        const { access_token } = req.body;
+
+        if (!access_token) {
+            return res.status(400).json({ error: "Access token is required" });
         }
-      }else{ //signup
-        console.log('email',email);
-        let username = await generateUsername(email);
-        console.log('username',username)
-        console.log('name',name)
-  
-        user = new User({
-          personal_info: { fullname: name, email, username },
-          google_auth: true
-        })
-  
-        await user.save().then((u) => {
-          user = u;
-        })
-        .catch(err => {
-          console.log('error',err);
-          return res.status(500).json({"error": err.message})
-        })
-      }
-  
-      return res.status(200).json(formatData(user));
-    })
-    .catch(err => {
-      return res.status(500).json({"error": "Failed to authenticate you with google. Try with some other google account" })
-    })
-})
+
+        const decodedUser = await getAuth().verifyIdToken(access_token);
+        const { email, name, picture } = decodedUser;
+        console.log(decodedUser);
+
+        let user = await User.findOne({ "personal_info.email": email }).select("personal_info.fullname personal_info.username personal_info.profile_img google_auth");
+        
+        if (user) {
+            if (!user.google_auth) {
+                return res.status(403).json({ error: "This email was signed up without google. Please log in with password to access the account" });
+            }
+        } else {
+            const username = await generateUsername(email);
+            console.log('Generated username:', username);
+
+            user = new User({
+                personal_info: { fullname: name, email, username },
+                google_auth: true
+            });
+
+            await user.save();
+        }
+
+        // Return user data in response
+        return res.status(200).json(formatData(user));
+    } catch (error) {
+        console.log('Error during google-auth:', error);
+        return res.status(500).json({ error: "Failed to authenticate you with google. Try with some other google account" });
+    }
+});
+
 
 server.post("/change-password", verifyJWT, (req, res) => {
 
